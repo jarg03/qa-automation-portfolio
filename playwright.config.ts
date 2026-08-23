c732cfdc8f40e65a5d13377eb14b71e920629364
Alexis Ramirez <alexisramirez@MacBook-Air-de-Alexis.local>
2026-08-22 21:57:42 -0600

    test: complete login suit (successful, wrong password, blocked user)

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+  baseURL: 'https://www.saucedemo.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -38,7 +38,7 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+    /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -47,7 +47,7 @@ export default defineConfig({
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    }, */
 
     /* Test against mobile viewports. */
     // {
@@ -67,7 +67,7 @@ export default defineConfig({
     // {
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    // }, 
   ],
 
   /* Run your local dev server before starting the tests */
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,9 +1,30 @@
 import { test, expect } from '@playwright/test';
 
-test('login exitoso con credenciales válidas', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com');
+test.describe('Login', () => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
+
+  test('login exitoso con credenciales válidas', async ({ page }) => {
     await page.getByPlaceholder('Username').fill('standard_user');
     await page.getByPlaceholder('Password').fill('secret_sauce');
-    await page.getByRole('button', { name: 'Login'}).click();
-    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page).toHaveURL('/inventory.html');
   });
+
+  test('login falla con contraseña incorrecta', async ({ page }) => {
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('contraseña_incorrecta');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page.getByText('Epic sadface: Username and password do not match any user in this service')).toBeVisible(); 
+  });
+
+  test('login falla con usuario bloqueado', async ({ page }) => {
+    await page.getByPlaceholder('Username').fill('locked_out_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page.getByText('Epic sadface: Sorry, this user has been locked out.')).toBeVisible(); 
+  });
+
+}); 
